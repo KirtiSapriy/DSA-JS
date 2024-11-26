e5081f19fbb6a123df888ab2bf7f92a8b6da9da2
KirtiSapriy <sapriyak01@gmail.com>
2024-11-26 18:02:23 +0530

    tsk

--- day-10/4.js
+++ day-10/4.js
@@ -3,9 +3,7 @@ function fibo(n) {
         if (n == 0) {
                 return n;
         }
-        if (n == 1) {
-                return n;
-        }
+   
 
         return fibo(n - 1) + fibo(n - 2)
 
